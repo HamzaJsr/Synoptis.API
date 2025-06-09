cc0737423210e1914f5eff6df5e23d7afed2d60e
HamzaJsr <gestiondev.j@gmail.com>
2025-06-09 17:26:27 +0200

    feat: ajout de la methode Update  dans l'interface

--- Services/Interfaces/IAppelOffreService.cs
+++ Services/Interfaces/IAppelOffreService.cs
@@ -14,6 +14,10 @@ namespace Synoptis.API.Services.Interfaces
         // Créer un nouvel AO à partir des données reçues du front en passant par les DTO
         Task<AppelOffreResponseDTO> CreateAppelOffreAsync(AppelOffreCreateDTO dto);
 
+        // Modifier un ao à partir de son id
+
+        Task<AppelOffreResponseDTO> UpdateAppelOffre(Guid id);
+
         // Supprimer un AO grace a son id 
         Task<AppelOffreResponseDTO?> DeleteAppelOffreAsync(Guid id);
 
